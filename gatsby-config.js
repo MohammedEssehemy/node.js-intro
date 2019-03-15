4aa3487fdbb75c77255c6182b29cb4a2a6a7b0e6
Mohammed Essehemy <mohammedessehemy@gmail.com>
2019-03-15 06:23:38 +0200

    in the name of ALLAH

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  siteMetadata: {
+    name: `Mohammed Essehemy`,
+    title: `Nodejs Intro`,
+    date: `March 15, 2019`,
+  },
+  plugins: [
+    `gatsby-plugin-layout`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `slides`,
+        path: `${__dirname}/src/slides`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-copy-images`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 1920,
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+};
